54e0495b29f0c626dee75e89651245fef9848b94
TimMotonaga <tim.motonaga@gmail.com>
2015-12-13 12:22:36 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,10 @@
+##A quick note on the plots: I started this class last month, but had to drop because of work. This is an 
+##improved version of the code I submitted last month; the new code uses lubridate and fixes an issue 
+##with read.csv which caused me to write extra code. The plots produced by the new code are the same as 
+##the plots produced by the old code, so gitbash did not commit them when I used "git commit -a".
+##That is why the plots say they were commited last month (though I created them with the current code, 
+##as you can verify).
+
 if(!file.exists("./household_power_consumption.txt")){
     tinyurl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     download.file(tinyurl,"./Power.zip")
